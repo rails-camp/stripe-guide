ed45328bd010b2b03483dd975046fc39db88eaad
Jordan Hudgens <jordan@wow.com>
2016-03-02 19:36:17 -0600

    Implemented subscriptions

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,24 @@
 class ChargesController < ApplicationController
   before_action :amount_to_be_charged
   before_action :set_description
+  before_action :set_plan
   before_action :authenticate_user!
 
   def new
   end
 
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail], 
-                                          stripe_token: params[:stripeToken])
-
-    charge = StripeTool.create_charge(customer_id: customer.id, 
+    if params[:subscription].include? 'yes'
+      StripeTool.create_membership(email: params[:stripeEmail], 
+                                   stripe_token: params[:stripeToken],
+                                   plan: @plan)
+    else
+      customer = StripeTool.create_customer(email: params[:stripeEmail], 
+                                            stripe_token: params[:stripeToken])
+      charge = StripeTool.create_charge(customer_id: customer.id, 
                                       amount: @amount,
                                       description: @description)
+    end
 
     redirect_to thanks_path
   rescue Stripe::CardError => e
@@ -26,10 +32,14 @@ class ChargesController < ApplicationController
   private
 
     def set_description
-      @description = "Some amazing product"
+      @description = "Basic Membership"
     end
 
     def amount_to_be_charged
-      @amount = 10000
+      @amount = 2999
+    end
+
+    def set_plan
+      @plan = 9999
     end
 end
--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -6,6 +6,14 @@ module StripeTool
     )
   end
 
+  def self.create_membership(email: email, stripe_token: stripe_token, plan: plan)
+    Stripe::Customer.create(
+      email: email,
+      source: stripe_token,
+      plan: plan
+    )
+  end
+
   def self.create_charge(customer_id: customer_id, amount: amount, description: description)
     Stripe::Charge.create(
       customer: customer_id,
